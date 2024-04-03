6bf3709fa097f44689704e6cbc897703522a6324
gambitbro <osofw12@gmail.com>
2024-04-03 15:41:21 +0900

    dd

--- cpp_src/chap4/ex4_1/main.cpp
+++ cpp_src/chap4/ex4_1/main.cpp
@@ -45,5 +45,21 @@ double Rectangle::getArea(void)
 
 int main()
 {
+    Circle donut;
+    Circle pizza(30);
+
+    cout << donut.getArea() << endl;
+
+    Circle *p;
+    p = &donut;
+
+    cout << p->getArea() << endl;
+    cout << (*p).getArea() << endl;
+
+    p = &pizza;
+
+    cout << p->getArea() << endl;
+    cout << (*p).getArea() << endl;
+
     return 0;
 }
