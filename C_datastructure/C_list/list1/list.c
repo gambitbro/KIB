485e3aca96ca873905364ee073fd4cb2ab2ece1f
gambitbro <67915790+gambitbro@users.noreply.github.com>
2024-06-19 17:08:36 +0900

    ㅇㅇㅇㅇ

--- C_datastructure/C_list/list1/list.c
+++ C_datastructure/C_list/list1/list.c
@@ -15,12 +15,35 @@ int main(void)
     ptr->next->data = 3;
     ptr->next->next->data = 4;
     ptr->next->next->next = NULL;
+{
+    Node *p = malloc(sizeof(Node));
+    p->data = 2;
+    p->next = ptr->next;
+    ptr->next = p;
+}
+{
+    Node *p = ptr->next->next;      //p가 3노드를 가리킴
+    ptr->next->next = p->next;
+    free(p);        //3노드 삭제 
+}
+    
+    //노드 삭제
+{
+    Node *p = ptr;
+    while(p){
+        Node *tmp = p;
+        p = p->next;
 
+        free(tmp);
+    }
+}
+
+    // 노드 하나씩 프린트
     Node *p = ptr;
     while(p != NULL){
         printf("%d", p->data);
         p = p->next;
-        
     }
+    printf("\n");
     return 0;
 }
